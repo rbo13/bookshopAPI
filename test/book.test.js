691f310b62ab3ca03ea926c99660a9d77967352a
whaangbuu <chardy.orofeo@yahoo.com.ph>
2017-07-28 13:41:59 +0800

    Added new test cases

--- test/book.test.js
+++ test/book.test.js
@@ -22,7 +22,7 @@ describe('Books', () => {
   * Test the /GET route
   */
   describe('/GET book', () => {
-      it('it should GET all the books', (done) => {
+      it('should GET all the books', (done) => {
         chai.request(server)
             .get('/book')
             .end((err, res) => {
@@ -32,6 +32,55 @@ describe('Books', () => {
               done();
             });
       });
+  });
+
+  /*
+  * Test the /POST route
+  */
+  describe('/POST book', () => {
+      it('should not POST a book without pages field', (done) => {
+          let book = {
+            title: "The Lord of the Rings",
+            author: "J.R.R Tolkien",
+            year: 1954
+          }
+
+          chai.request(server)
+            .post('/book')
+            .send(book)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('errors')
+                res.body.errors.should.have.property('pages');
+                res.body.errors.pages.should.have.property('kind').eql('required');
+                done();
+            });
+      });
+
+      it('should POST a book', () => {
+        let book = {
+          title: "The Lord of the Rings",
+          author: "J.R.R Tolkien",
+          year: 1954,
+          pages: 1170
+        }
+
+        chai.request(server)
+          .post('/book')
+          .send(book)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property("message").eql("Book added");
+            res.body.book.should.have.property('title');
+            res.body.book.should.have.property('author');
+            res.body.book.should.have.property('pages');
+            res.body.book.should.have.property('year');
+          });
+
+
+      })
   });
 
 });
